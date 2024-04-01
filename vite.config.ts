c789b6e30270f0c27099c482c7f52e5c1c3d4bf0
andy <570678569@qq.com>
2024-04-01 08:58:57 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port:5210,
+    proxy: {
+      "/api": {
+        target: "https://xxxxxxx.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react()],
 })
