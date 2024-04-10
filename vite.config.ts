075713c756f54fe8ee5cf762d233f1d7dfee6ce8
andy <570678569@qq.com>
2024-04-10 16:49:15 +0800

    optimize vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port:5210,
+    port: 5210,
     proxy: {
       "/api": {
         target: "https://xxxxxxx.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: "es2015",
+    assetsDir: "static",
+  },
+});
